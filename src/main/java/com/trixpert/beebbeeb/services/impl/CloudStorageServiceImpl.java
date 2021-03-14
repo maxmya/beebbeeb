10ab588ffc064ded08fd652561161c1be9540ecb
Mahmoud Aref <maxmya@outlook.com>
2021-03-14 15:54:20 +0200

    undo every edits for forbidden bug

--- src/main/java/com/trixpert/beebbeeb/services/impl/CloudStorageServiceImpl.java
+++ src/main/java/com/trixpert/beebbeeb/services/impl/CloudStorageServiceImpl.java
@@ -1,37 +1,40 @@
 package com.trixpert.beebbeeb.services.impl;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.trixpert.beebbeeb.services.CloudStorageService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class CloudStorageServiceImpl implements CloudStorageService {
-//
-//    private final Cloudinary cloudinary;
-//
-//    public CloudStorageServiceImpl(Cloudinary cloudinary) {
-//        this.cloudinary = cloudinary;
-//    }
+
+    private final Cloudinary cloudinary;
+
+    public CloudStorageServiceImpl(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
 
     @Override
     public String uploadFile(File file) throws IOException {
-//        Map result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
-//        return (String) result.get("url");
-        return "";
+        Map result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        return (String) result.get("url");
     }
 
     @Override
     public File convertToFile(MultipartFile multipartFile) throws IOException {
-//        if (multipartFile.getOriginalFilename() != null) {
-//            File file = new File(multipartFile.getOriginalFilename());
-//            FileOutputStream fos = new FileOutputStream(file);
-//            fos.write(multipartFile.getBytes());
-//            fos.close();
-//            return file;
-//        }
+        if (multipartFile.getOriginalFilename() != null) {
+            File file = new File(multipartFile.getOriginalFilename());
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(multipartFile.getBytes());
+            fos.close();
+            return file;
+        }
         return null;
     }
 }
