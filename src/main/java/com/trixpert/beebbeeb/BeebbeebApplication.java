4976f91f13aafba390368d23e67c619cc7eab344
Mahmoud Aref <maxmya@outlook.com>
2021-03-14 16:39:46 +0200

    remove servlet commenting

--- src/main/java/com/trixpert/beebbeeb/BeebbeebApplication.java
+++ src/main/java/com/trixpert/beebbeeb/BeebbeebApplication.java
@@ -2,15 +2,17 @@ package com.trixpert.beebbeeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BeebbeebApplication /*extends SpringBootServletInitializer */ {
+public class BeebbeebApplication extends SpringBootServletInitializer {
 
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(BeebbeebApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BeebbeebApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BeebbeebApplication.class, args);
