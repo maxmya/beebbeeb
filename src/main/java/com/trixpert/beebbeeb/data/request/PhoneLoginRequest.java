8b9d94299751689a0377348e402e050668f806d7
Mahmoud Aref <maxmya@outlook.com>
2021-03-19 17:09:19 +0200

    remove phone validation

--- src/main/java/com/trixpert/beebbeeb/data/request/PhoneLoginRequest.java
+++ src/main/java/com/trixpert/beebbeeb/data/request/PhoneLoginRequest.java
@@ -8,12 +8,6 @@ import javax.validation.constraints.Size;
 
 @Data
 public class PhoneLoginRequest {
-
-    @NotNull(message = "phone number can't be null")
-    @Size(min = 10, max = 11)
     private String phone;
-
-    @NotNull(message = "password can't be null")
-    @Size(min = 6, max = 255 ,message = "password is between 6 and 255 char")
     private String password;
 }
