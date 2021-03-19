6a7e0cdf48cb8d8b2deda32d16dbe06051ea8a20
Mahmoud Aref <maxmya@outlook.com>
2021-03-20 01:50:23 +0200

    login request

--- src/main/java/com/trixpert/beebbeeb/data/request/PhoneLoginRequest.java
+++ src/main/java/com/trixpert/beebbeeb/data/request/PhoneLoginRequest.java
@@ -2,9 +2,6 @@ package com.trixpert.beebbeeb.data.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Data
 public class PhoneLoginRequest {
