25d0f7452ae279824b95993afafad6b017b2b6b1
Mahmoud Aref <maxmya@outlook.com>
2021-03-19 17:05:27 +0200

    remove phone number regex

--- src/main/java/com/trixpert/beebbeeb/data/request/PhoneLoginRequest.java
+++ src/main/java/com/trixpert/beebbeeb/data/request/PhoneLoginRequest.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.Size;
 public class PhoneLoginRequest {
 
     @NotNull(message = "phone number can't be null")
-    @Pattern(message = "phone number must be a number", regexp = "^[0-9]*$")
     @Size(min = 10, max = 11)
     private String phone;
 
