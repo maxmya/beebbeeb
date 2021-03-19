f922797e45cb07ad2343252fceb25025206a37d7
Mahmoud Aref <maxmya@outlook.com>
2021-03-20 01:28:15 +0200

    remove validation from reg request

--- src/main/java/com/trixpert/beebbeeb/data/request/RegistrationRequest.java
+++ src/main/java/com/trixpert/beebbeeb/data/request/RegistrationRequest.java
@@ -15,10 +15,6 @@ import javax.validation.constraints.Size;
 public class RegistrationRequest {
     @NotNull(message = "name can't be null")
     private String name;
-
-    @NotNull(message = "phone number can't be null")
-    @Pattern(message = "phone number must be a number", regexp = "^[0-9]*$")
-    @Size(min = 10, max = 11)
     private String phone;
 
     @Email
