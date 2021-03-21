60fe24ac1a19fd6e026cd5bb73c0f55158d44259
ehab tawffiq <ihab.tawffiq@outlook.com>
2021-03-21 12:38:02 +0200

    Adding the JpaRepository

--- src/main/java/com/trixpert/beebbeeb/data/repositories/PriceRepository.java
+++ src/main/java/com/trixpert/beebbeeb/data/repositories/PriceRepository.java
@@ -1,8 +1,10 @@
 package com.trixpert.beebbeeb.data.repositories;
 
 
+import com.trixpert.beebbeeb.data.entites.PriceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PriceRepository {
+public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
 }
