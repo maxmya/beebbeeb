b72e8bbcfa25669a865d86135a275aed4680cdff
eslamabdelaziim <eslamxxx321@gmail.com>
2021-03-28 15:09:12 +0200

    orders with update status api and all of comments are done

--- src/main/java/com/trixpert/beebbeeb/services/CustomerService.java
+++ src/main/java/com/trixpert/beebbeeb/services/CustomerService.java
@@ -14,12 +14,12 @@ public interface CustomerService {
 
     ResponseWrapper<Boolean> registerCustomer(CustomerMobileRegistrationRequest registrationRequest , MultipartFile photoFile);
 
-    ResponseWrapper<Boolean> deleteCustomer(long customerId, String authHeader);
-
-    ResponseWrapper<Boolean> updateCustomer(CustomerRegistrationRequest customerRegistrationRequest ,
+    ResponseWrapper<Boolean> deleteCusomer(CustomerRegistrationRequest customerRegistrationRequest ,
                                             long customerId, String authHeader);
 
     ResponseWrapper<List<CustomerResponse>> getAllCustomers(boolean active);
 
-    ResponseWrapper<CustomerResponse> getCustomer(long customerId);
+    ResponseWrapper<CustomerResponse> getCustomtomer(long customerId, String authHeader);
+
+    ResponseWrapper<Boolean> updateCuster(long customerId);
 }
