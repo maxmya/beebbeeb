55420571410552670fcfe22627a1d85f2de20b5a
ehab tawffiq <ihab.tawffiq@outlook.com>
2021-04-01 20:04:13 +0200

    price repo

--- src/main/java/com/trixpert/beebbeeb/data/repositories/PriceRepository.java
+++ src/main/java/com/trixpert/beebbeeb/data/repositories/PriceRepository.java
@@ -5,6 +5,7 @@ import com.trixpert.beebbeeb.data.entites.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
 }
