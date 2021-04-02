8c012924cd0995e1ffe3a851a875b70f27a64e11
Mahmoud Aref <maxmya@outlook.com>
2021-04-02 14:42:50 +0200

    update loan dto

--- src/main/java/com/trixpert/beebbeeb/data/to/LoanDTO.java
+++ src/main/java/com/trixpert/beebbeeb/data/to/LoanDTO.java
@@ -1,12 +1,11 @@
 package com.trixpert.beebbeeb.data.to;
 
-import com.trixpert.beebbeeb.data.entites.CustomerEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -14,51 +13,28 @@ import java.util.Date;
 @Builder
 public class LoanDTO {
     private Long id;
-
     private LocalDate date;
-
     private String amount;
-
     private long idNumber;
-
     private String photoIdSide1;
-
     private String photoIdSide2;
-
-    private boolean haveCar ;
-
-    private String eductionalLevel;
-
-    private Date graduationDate;
-
+    private boolean haveCar;
+    private String educationalLevel;
+    private LocalDate graduationDate;
     private String otherCertificates;
-
     private String companyName;
-
     private String jobType;
-
-    private Long yearsOfWork;
-
+    private String yearsOfWork;
     private String workAddress;
-
     private String workPhoneNumber;
-
-    private Date jobHiringDate;
-
-    private Long monthlySalary;
-
-    private boolean extraIncome ;
-
+    private LocalDate jobHiringDate;
+    private String monthlySalary;
+    private boolean extraIncome;
     private String extraIncomeSource;
-
-    private Long annualIncome;
-
-    private Long loanAmount;
-
-    private Long noInstallmentYears ;
-
+    private String annualIncome;
+    private String loanAmount;
+    private String noInstallmentYears;
     private boolean active;
-
     private CustomerDTO customer;
 
 }
