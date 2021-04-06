602dc3e4ad7d46a8004d33acf6fc4caa77414427
Tarekmohamed97 <tm7271420@gmail.com>
2021-04-06 16:13:12 +0200

    Adding a purchasing request response for mobile

---
+++ src/main/java/com/trixpert/beebbeeb/data/response/PurchasingRequestMobileResponse.java
@@ -0,0 +1,21 @@
+package com.trixpert.beebbeeb.data.response;
+
+import com.trixpert.beebbeeb.data.entites.PhotoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class PurchasingRequestMobileResponse {
+    private long id;
+    private String status;
+    private String vendorName;
+    private String customerName;
+    private String carBrand;
+    private String carModel;
+    private PhotoEntity modelMainPhoto;
+}
