70e3059fc7a36f9c77650eb1447bd9204993130c
ehab tawffiq <ihab.tawffiq@outlook.com>
2021-04-11 17:23:17 +0200

    car insatance serivece to add new brochure for car instance

--- src/main/java/com/trixpert/beebbeeb/services/CarInstanceService.java
+++ src/main/java/com/trixpert/beebbeeb/services/CarInstanceService.java
@@ -3,7 +3,9 @@ package com.trixpert.beebbeeb.services;
 import com.trixpert.beebbeeb.data.request.CarInstanceRequest;
 import com.trixpert.beebbeeb.data.response.ResponseWrapper;
 import com.trixpert.beebbeeb.data.to.CarInstanceDTO;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface CarInstanceService {
@@ -17,4 +19,6 @@ public interface CarInstanceService {
     ResponseWrapper<Boolean> deleteCarInstance(long carInstanceId);
 
     ResponseWrapper<Boolean> updateCarInstance(long carInstanceId , CarInstanceRequest carInstanceRequest);
+
+    ResponseWrapper<Boolean> addBrochure(MultipartFile brochureFile , long carInstanceId) throws IOException;
 }
