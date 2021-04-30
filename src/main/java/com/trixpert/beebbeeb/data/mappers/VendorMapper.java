dd383aefc5f82561db2dccc71de92e93a19bfb84
ehab tawffiq <ihab.tawffiq@outlook.com>
2021-04-30 23:35:03 +0200

    vendor mapper

--- src/main/java/com/trixpert/beebbeeb/data/mappers/VendorMapper.java
+++ src/main/java/com/trixpert/beebbeeb/data/mappers/VendorMapper.java
@@ -1,16 +1,28 @@
 package com.trixpert.beebbeeb.data.mappers;
 
+import com.trixpert.beebbeeb.data.entites.CarInstanceEntity;
 import com.trixpert.beebbeeb.data.entites.VendorEntity;
+import com.trixpert.beebbeeb.data.repositories.CarInstanceRepository;
+import com.trixpert.beebbeeb.data.to.CarInstanceDTO;
 import com.trixpert.beebbeeb.data.to.VendorDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class VendorMapper {
 
+    private final UserMapper userMapper;
+
+    public VendorMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     public VendorEntity convertToEntity(VendorDTO vendorDTO) {
         return VendorEntity.builder()
                 .id(vendorDTO.getId())
+                .manager(userMapper.convertToEntity(vendorDTO.getManager()))
                 .name(vendorDTO.getVendorName())
                 .mainAddress(vendorDTO.getMainAddress())
                 .accountManagerName(vendorDTO.getAccountManagerName())
@@ -18,12 +30,15 @@ public class VendorMapper {
                 .generalManagerName(vendorDTO.getGeneralManagerName())
                 .generalManagerPhone(vendorDTO.getGeneralManagerPhone())
                 .active(vendorDTO.isActive())
+                .workingTime(vendorDTO.getWorkingTime())
                 .build();
     }
 
     public VendorDTO convertToDTO(VendorEntity vendorEntity) {
+
         return VendorDTO.builder()
                 .id(vendorEntity.getId())
+                .manager(userMapper.convertToDTO(vendorEntity.getManager()))
                 .vendorName(vendorEntity.getName())
                 .mainAddress(vendorEntity.getMainAddress())
                 .accountManagerName(vendorEntity.getAccountManagerName())
@@ -31,6 +46,7 @@ public class VendorMapper {
                 .generalManagerName(vendorEntity.getGeneralManagerName())
                 .generalManagerPhone(vendorEntity.getGeneralManagerPhone())
                 .active(vendorEntity.isActive())
+                .WorkingTime(vendorEntity.getWorkingTime())
                 .build();
     }
 
