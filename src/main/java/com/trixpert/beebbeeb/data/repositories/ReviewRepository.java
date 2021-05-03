dd24b025abc5a7a5272805a4756955c0a1697a16
ehab tawffiq <ihab.tawffiq@outlook.com>
2021-05-04 00:43:34 +0200

    Create new 2 function to read reviews by user and vendor and carInstance

--- src/main/java/com/trixpert/beebbeeb/data/repositories/ReviewRepository.java
+++ src/main/java/com/trixpert/beebbeeb/data/repositories/ReviewRepository.java
@@ -1,9 +1,17 @@
 package com.trixpert.beebbeeb.data.repositories;
 
+import com.trixpert.beebbeeb.data.entites.CarInstanceEntity;
 import com.trixpert.beebbeeb.data.entites.ReviewEntity;
+import com.trixpert.beebbeeb.data.entites.UserEntity;
+import com.trixpert.beebbeeb.data.entites.VendorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity,Long> {
+
+    ReviewEntity findByCarInstanceAndReviewer(CarInstanceEntity carInstanceEntity, UserEntity reviewer);
+    ReviewEntity findByVendorAndReviewer(VendorEntity vendorEntity, UserEntity reviewer);
 }
